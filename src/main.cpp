f82bf8b5d1e11cb8707523fcaead7b132b697cbc
Codinq <johnpaulsen2005@gmail.com>
2022-02-08 19:14:09 +0100

    works v0.0.1 - no config *yet*

--- src/main.cpp
+++ src/main.cpp
@@ -134,6 +134,7 @@ MAKE_HOOK_MATCH(ResultsViewController_Restart, &ResultsViewController::RestartBu
 
     UnityEngine::GameObject::Destroy(combo->get_gameObject());
     UnityEngine::GameObject::Destroy(avg->get_gameObject());
+    UnityEngine::GameObject::Destroy(scoreTextPercentage->get_gameObject());
 
     ResultsViewController_Restart(self);
 
@@ -143,6 +144,7 @@ MAKE_HOOK_MATCH(ResultsViewController_Continue, &ResultsViewController::Continue
 
     UnityEngine::GameObject::Destroy(combo->get_gameObject());
     UnityEngine::GameObject::Destroy(avg->get_gameObject());
+    UnityEngine::GameObject::Destroy(scoreTextPercentage->get_gameObject());
 
     ResultsViewController_Continue(self);
 
